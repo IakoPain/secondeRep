cbcbf5301179316c252004dc486884ff9de53a0d
IakoPain <95188886+IakoPain@users.noreply.github.com>
2024-06-06 09:44:01 +0200

    Delete Géolocalisation directory
    bruvw

--- Géolocalisation/module2.py
+++
@@ -1,9 +0,0 @@
-import folium
-
-macarte = folium.Map(location=[42.838019, -0.14027])
-folium.Marker([42.85089, -0.139836], popup="Départ", icon=folium.Icon(color='green')).add_to(macarte)
-folium.Marker([42.842588, -0.140455], popup="Pause déjeuner").add_to(macarte)
-folium.Marker([42.83491, -0.140502], popup="Pause déjeuner").add_to(macarte)
-folium.Marker([42.828154, -0.139286], popup="Pause les pieds dans l’eau").add_to(macarte)
-folium.Marker([42.823732, -0.13782], popup="Arrivée", icon=folium.Icon(color='red')).add_to(macarte)
-macarte.save("carterando.html")
